ac0bef44f891670b19eddde6c72b020e4793c102
Waseel Fawad <waseelfawad@gmail.com>
2023-05-09 18:34:05 +0500

    feat(Tailwind): added tailwind css for Application

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
       <div className="card">
         <button
           type="button"
